359ec7b1a92faa6512971e004f0364da444e544a
Itunuoluwa Fadiyimu <itunufadiyimu@gmail.com>
2025-10-25 03:06:31 +0100

    feat: Ensure GitHub callback URL is always configured from environment variable

--- server/auth.ts
+++ server/auth.ts
@@ -50,9 +50,7 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-      callbackURL:
-        process.env.GITHUB_CALLBACK_URL ||
-        "http://localhost:3000/api/auth/callback/github",
+      callbackURL: process.env.GITHUB_CALLBACK_URL!,
       scope: ["user", "repo"],
     },
     async (
